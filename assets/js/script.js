19cd76adb8ddae9e1811eefc26b0cf4de69623e5
DucPM <minhducpham.it@gmail.com>
2022-08-12 06:45:46 +0700

    update

---
+++ assets/js/script.js
@@ -0,0 +1,89 @@
+
+var app = app || {};
+
+app.init = function () {
+    app.tab();
+    app.anchorLink();
+    app.showMenu();
+    app.jsSlider();
+    app.productList();
+};
+
+app.tab = function () {
+    $(document).on("click", ".tab a", function (e) {
+        e.preventDefault();
+        let target = $(this).attr("href").split('#')[1];
+
+        $(this).parent().addClass("active").siblings().removeClass("active");
+        $('[data-id="' + target + '"]').fadeIn(0).siblings().fadeOut(0);
+        history.pushState({}, '', '#' + target);
+    });
+
+    if (location.hash && $(".tab li a[href='" + location.hash + "']").length) {
+        $(".tab li a[href='" + location.hash + "']").trigger("click");
+
+        $('.pagination a.page-numbers').each(function (i, a) {
+            $(a).attr('href', $(a).attr('href') + '#' + $(a).parents('.tab-box').attr('data-id'));
+        });
+    } else {
+        $(".tab li:first-child a").trigger("click");
+        history.replaceState(null, null, ' ');
+    }
+}
+
+app.anchorLink = function () {
+    $('.anchor-link').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+}
+
+app.showMenu = function () {
+    let ele = $(".hamburger-box");
+    ele.on('click', function (e) {
+        $(this).toggleClass('is-active');
+        $('html').toggleClass('is-active');
+        $('.p-header').toggleClass('is-open');
+    });
+}
+
+app.jsSlider = function () {
+    $('.js-mainvisual').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        arrows: false,
+    });
+}
+
+app.productList = function () {
+    $('.js-product-slider').slick({
+        dots: false,
+        arrows: true,
+        // autoplay: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+    })
+}
+
+$(document).ready(function () {
+
+    $('.page-top a').click(function () {
+        $('html, body').animate({ scrollTop: 0 });
+        return false;
+    });
+
+    app.init();
+});
